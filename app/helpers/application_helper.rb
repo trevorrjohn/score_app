fde68afedf3d9fa259dee1e5fff93b2fe13b95ba
Trevor John <trevorrjohn@gmail.com>
2011-11-15 17:22:07 -0500

    stylesheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #return title on per page basis
+  def title
+    base_title = "Score-App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
