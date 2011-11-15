1cc967ed08c7441e2008359b4fb963b17ddd2925
Trevor John <trevorrjohn@gmail.com>
2011-11-15 16:46:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScoreApp::Application.config.secret_token = 'ad6aed0660afc4add9d7299d37bcd20a9ee59213322f930ba8904b91058d3da9fc4e2a28f9e534491823abde5822f80e985ca0871db5d3323377fa6a7a1bfe44'
