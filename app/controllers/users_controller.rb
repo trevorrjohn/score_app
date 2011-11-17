5771119a4f294bf367e0b99eee0b95e4fca405c4
Trevor John <trevorrjohn@gmail.com>
2011-11-16 21:48:05 -0500

    user and admin created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update, :show]
+  before_filter :correct_user, :only => [:edit, :update, :show]
+  before_filter :admin_user,   :only => [:destroy, :index]
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:sucess] = "User destroyed."
+    redirect_to users_path
+  end
+
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
+
   def new
     @user = User.new
     @title = "Sign up"
@@ -20,4 +35,43 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @title = "Edit user"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      user = @user
+      session[:user_id] = user.id
+      sign_in @user
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      puts @user.admin?
+      if !current_user?(@user)
+        if !current_user.admin?
+          flash[:success] = "You are unautharized to view/edit other users."
+          redirect_to current_user
+        end
+      end
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
